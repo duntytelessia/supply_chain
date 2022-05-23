10897e539b34d8797af5e6f87fd717653ad9ebf7
Wenjie LIU <wenjie.liu1002@gmail.com>
2022-05-23 16:44:58 +0200

    Order between control tower and distributor

--- PycharmProjects/django_Projects/Projet_1/week/urls.py
+++ PycharmProjects/django_Projects/Projet_1/week/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('<int:week>/<str:username>/stock/Suppliers_A', stock_supp_a, name='stock_supp_a'),
     path('<int:week>/controltower/endpoint1', modify_as_controltower, name='modify_as_controltower'),
     path('<int:week>/controltower/endpoint2', modify_as_controltower, name='modify_as_controltower'),
+    path('<int:week>/controltower/endpoint3', modify_as_controltower, name='modify_as_controltower'),
+
 ]
--- PycharmProjects/django_Projects/Projet_1/week/views.py
+++ PycharmProjects/django_Projects/Projet_1/week/views.py
@@ -54,6 +54,13 @@ def modify_as_controltower(request, week):
     dict_b = {}
     keys_b = []
 
+    distributors = User.objects.filter(groups__name__exact='Distributors')
+    goods_a1 = Goods.objects.filter(idG__in=['F1', 'F2'])
+    buyer1 = request.user
+    list_a1 = []
+    dict_a1 = {}
+    keys_a1 = []
+
     # form layout
     def formlayout(formset, keys, dict):
         i = 0
@@ -84,9 +91,23 @@ def modify_as_controltower(request, week):
             list_b.append(id)
             keys_b.append(buyer.codename + good.idG)
 
+    for seller in distributors:
+        for good in goods_a1:
+            id = seller.codename + buyer1.codename + good.idG + str(week.week)
+            if Order.objects.filter(idO__exact=id).exists():
+                order = Order.objects.get(idO__exact=id)
+            else:
+                order = Order(idO=id, sellerO=seller, goods=good, buyerO=buyer, dateO=week)
+            order.save()
+            list_a1.append(id)
+            keys_a1.append(seller.codename + good.idG)
+
+
     # form creation
     TransactionFormSet = modelformset_factory(Transaction, fields=['quanT', 'priceT'],
                                               labels={'quanT': 'Q', 'priceT': 'P'}, extra=0)
+    OrderFormSet = modelformset_factory(Order, fields=['quanO'],
+                                              labels={'quanO': 'Q'}, extra=0)
 
     if request.method == 'POST':
         if 'submitA' in request.POST:
@@ -101,10 +122,18 @@ def modify_as_controltower(request, week):
                 formset_b.save()
                 messages.success(request, 'Transaction edited')
                 return HttpResponseRedirect(request.path_info)
+        if 'submitA1' in request.POST:
+            formset_a1 = OrderFormSet(request.POST, queryset=Order.objects.filter(idO__in=list_a1))
+            if formset_a1.is_valid():
+                formset_a1.save()
+                messages.success(request, 'Order edited')
+                return HttpResponseRedirect(request.path_info)
+
 
     else:
         formset_a = TransactionFormSet(queryset=Transaction.objects.filter(idT__in=list_a))
         formset_b = TransactionFormSet(queryset=Transaction.objects.filter(idT__in=list_b))
+        formset_a1 = OrderFormSet(queryset=Order.objects.filter(idO__in=list_a1))
     context = {
         'suppliers_a': suppliers_a,
         'suppliers_b': suppliers_b,
@@ -114,13 +143,17 @@ def modify_as_controltower(request, week):
         'dict_a': dict_a,
         'dict_b': dict_b,
         'messages': messages,
+        'distributors': distributors,
+        'goods_a1': goods_a1,
+        'dict_a1': dict_a1,
     }
-    #if formsetA:
     formlayout(formset_a, keys_a, dict_a)
     context.update({'formset_a': formset_a})
-    #if formsetB:
     formlayout(formset_b, keys_b, dict_b)
     context.update({'formset_b': formset_b})
+    formlayout(formset_a1, keys_a1, dict_a1)
+    context.update({'formset_a1': formset_a1})
+
 
     return render(request, 'week/modify_as_controltower.html', context=context)
 
